cc214bf46ab692bd4f30d3de57d6bc7a45d72d1a
Rakib0101 <rakib.01jan@gmail.com>
2022-02-08 21:20:21 +0600

    update: mobile menu

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+function mobileMenu() {
+    console.log("clicked");
+    const mobileMenu = document.querySelector(".navbar__menu");
+    if (mobileMenu.style.display === "block") {
+        mobileMenu.style.display = "none";
+    } else {
+        mobileMenu.style.display = "block";
+    }
+}
