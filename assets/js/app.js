64d231103d46831a1a7c8c232e7de614ab09f945
Rakib0101 <rakib.01jan@gmail.com>
2022-02-09 22:56:53 +0600

    update: slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,24 +17,3 @@ function mobileMenu() {
 AOS.init({
         easing: 'ease-in-out-sine'
 });
-const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "vertical",
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: ".swiper-scrollbar",
-    },
-});
