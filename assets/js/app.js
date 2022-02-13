a11042bdf6a50527219812d2a7c60d45bdc2101b
dev-nasreen <nasreen.akter34@gmail.com>
2022-02-13 02:57:02 -0800

    fixin: px perfect

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,23 +25,21 @@ AOS.init({
  new Swiper(".mySwiper", {
         loop:true,
         slidesPerView: 1,
-        spaceBetween: 10,
+        spaceBetween: 16,
         pagination: {
+          dynamicBullets: true,
           el: ".swiper-pagination",
           clickable: true,
         },
         breakpoints: {
           640: {
             slidesPerView: 1,
-            spaceBetween: 20,
           },
           768: {
             slidesPerView: 2,
-            spaceBetween: 40,
           },
           1024: {
             slidesPerView: 3,
-            spaceBetween: 50,
           },
         },
       });
